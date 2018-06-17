ad9d6fbd30e8ab79a195f2e7f1418b0d585a42bf
YR Fang <fyr1116@gmail.com>
2018-06-17 10:29:27 +0800

    nodemon config and backend with routes

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,10 @@ const About = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     <section className="page_layout">
-      <h1>About this project</h1>
+      <h1>About this project!!!</h1>
+      <p>
+        The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use seasonal ingredients.
+      </p>
     </section>
   </div>
   
